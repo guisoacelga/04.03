fc2b98625309110ba35692e5363f10568c917dfa
guisoacelga <guiso.acelga@gmail.com>
2021-03-05 19:12:36 +0100

    improved output

--- src/FindFactorDemo.java
+++ src/FindFactorDemo.java
@@ -2,14 +2,16 @@ public class FindFactorDemo {
     public static void main(String[] args) {
 
         findFactors(35);
+        System.out.println("");
         findFactors(57);
+        System.out.println("");
         findFactors(22);
 
     }
 
     public static void findFactors (int number) {
 
-        System.out.print("\nFaktoren von " + number + ": ");
+        System.out.print("Faktoren von " + number + ": ");
 
         int counter = 1;
         while (counter <= number) {
