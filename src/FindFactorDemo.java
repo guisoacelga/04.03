09b6cf3fdf100c77ff7451740d699f619104f78e
guisoacelga <guiso.acelga@gmail.com>
2021-03-05 19:14:31 +0100

    improved output

--- src/FindFactorDemo.java
+++ src/FindFactorDemo.java
@@ -25,7 +25,7 @@ public class FindFactorDemo {
             {
                 System.out.print(counter + ".");
             }
-            //System.out.println("");
+
             counter++;
 
         }
