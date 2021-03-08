25845f536475a066d3cb783b5a2add985085c5c4
guisoacelga <guiso.acelga@gmail.com>
2021-03-08 02:43:07 +0100

    refactored

--- src/BruttoNettoSwitch.java
+++ src/BruttoNettoSwitch.java
@@ -1,18 +1,25 @@
-public class BruttoNettoTaxClass {
-
+public class BruttoNettoSwitch {
     public static void  main(String[] args) {
 
         float netPrice = 451;
         int taxClass = 3;
         int taxRate = 0;
 
-        if (taxClass == 1) {
+        switch (taxClass) {
+            case 1:
+                taxRate = 20;
+            case 2:
+                taxRate = 16;
+            case 3:
+                taxRate = 8;
+        }
+      /*  if (taxClass == 1) {
             taxRate = 20;
         } else if (taxClass == 2) {
             taxRate = 16;
         } else if (taxClass == 3) {
             taxRate = 8;
-        }
+        }*/
 
         float taxApplied = (netPrice * taxRate)/100;
         float result = netPrice + taxApplied;
