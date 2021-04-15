d9f11dd804cafcbca2c50f82eddd76d1a095f396
guiso.acelga <guiso.acelga@gmail.com>
2021-04-15 10:04:56 +0200

    refactored

--- src/DistanceDemo.java
+++ src/DistanceDemo.java
@@ -2,13 +2,13 @@ public class DistanceDemo {
     public static void  main(String[] args) {
         int kilometerValue = 10;
 
-        double miles = kilometerToMiles(kilometerValue);
+        double miles = convertKilometerToMiles(kilometerValue);
         System.out.println(kilometerValue + "kilometers are " + miles + " miles." );
 
     }
 
 
-    public static double  kilometerToMiles(int kilometer) {
+    public static double  convertKilometerToMiles(int kilometer) {
      double kilo = kilometer / 1.609;
      return kilo;
 
